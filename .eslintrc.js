ac518eb84511acab5e426e283ee73ab2629712f6
Lucas <ltonussi@deitres.com>
2026-06-22 10:42:31 -0300

    feat: add linting configuration and semantic versioning automation script

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  extends: '@react-native',
+  rules: {
+    semi: ['error', 'never'],
+    quotes: ['error', 'single'],
+    'react-hooks/exhaustive-deps': 'off',
+  },
+}
