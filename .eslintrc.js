ef7fd417e9d1e080e56116a7795e2564fd4f7865
Lucas <ltonussi@deitres.com>
2026-06-22 10:57:19 -0300

    chore: update tsconfig formatting and exclude minor.js from compilation

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  root: true,
-  extends: '@react-native',
-  rules: {
-    semi: ['error', 'never'],
-    quotes: ['error', 'single'],
-    'react-hooks/exhaustive-deps': 'off',
-  },
-}
